e38424b998bba8bc4f87ccd402923cf046abdd7d
Randeep Chauhan <50373889+randeeprajputr@users.noreply.github.com>
2020-11-06 13:27:52 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,93 @@
+//importing
+import express from 'express';
+import mongoose from 'mongoose';
+import Messages from './dbMessages.js';
+import Pusher from "pusher";
+import cors from 'cors';
+//app config
+const app = express();
+const port=process.env.PORT ||9000;
+
+
+const pusher=new Pusher({
+    appId:'1088250',
+    key:'d026d50ae1a9f3614fd7',
+    secret:'2811bd0415c36c4a4edd',
+    cluster:'ap2',
+    encrypted:'true'
+});
+
+//middlewares
+app.use(express.json());
+app.use(cors());
+
+app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers","*");
+    next();
+})
+
+//DB config
+const connection_url="mongodb+srv://admin:dU4nQMjpIUHKVZni@cluster0.5aolf.mongodb.net/chattingdb?retryWrites=true&w=majority";
+
+mongoose.connect(connection_url,
+{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true
+});
+
+const db =mongoose.connection;
+
+db.once("open",()=>{
+    console.log("DB connected");
+    const msgCollection=db.collection("messagecontents");
+    const changeStream=msgCollection.watch();
+
+    changeStream.on("change",(change)=>{
+        console.log(change);
+
+        if(change.operationType==='insert'){
+            const messageDetails=change.fullDocument;
+            pusher.trigger('messages','inserted',{
+                name:messageDetails.name,
+                message:messageDetails.message,
+                timestamp:messageDetails.timestamp,
+                received:messageDetails.received,
+            });
+        }else{
+            console.log("Error triggring Pusher")
+        }
+
+    });
+});
+// ????
+
+
+
+//api routes
+app.get('/',(req,res)=>res.status(200).send('hello world'));
+app.get('/messages/sync',(req,res)=>{
+    Messages.find((err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        } else{
+            res.status(200).send(data)
+        }
+    })
+})
+app.post("/messages/new",(req,res)=>{
+    const dbMessage=req.body
+
+    Messages.create(dbMessage,(err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }else{
+            res.status(201).send(`new message created:\n${data}`)
+        }
+    })
+})
+
+//listen
+app.listen(port,()=>console.log(`Listening on localhost:${port}`));
+
